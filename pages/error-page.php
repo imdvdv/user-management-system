7e06c52e7a3fbfb5e5d4c570279518f6320395ab
Igor Medvedev <vinnepooh55@gmail.com>
2024-05-06 18:39:53 +0600

    404 page replaced with error page

--- pages/error-page.php
+++ pages/error-page.php
@@ -7,14 +7,14 @@ include_once __DIR__ . "/../src/config/env.php";
 <?php require_once __DIR__ . "/../public/components/head.html";?>
 <body>
     <div class="container">
-        <h3 class="title" style="text-decoration-color: #d93025">404</h3>
+        <h3 class="title" style="text-decoration-color: #d93025">Error</h3>
         <div class="message failure show">
             <p class="message-text">
                 <?php
                 if(isset($_SESSION["message"]) && !empty($_SESSION["message"])) {
                     echo $_SESSION["message"];
                 } else{
-                    echo "NOT FOUND";
+                    echo "Something went wrong. Please try again later.";
                 }
                 unset($_SESSION["message"]);
                 ?>
